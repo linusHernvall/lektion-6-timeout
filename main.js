63e4f810f728d879570553b095e13b0f9416e5b2
Linus Hernvall <linus.hernvall@gmail.com>
2022-11-15 09:54:56 +0100

    add button to cancel background change

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 window.addEventListener('DOMContentLoaded', main); // main är en callback-funktion därför inga paranteser.
 
+let timeoutRef;
+
 function main() {
     // Start på programmet.
-
-    setTimeout(changeBackgroundColor, 4000);
+   const ref = setTimeout(changeBackgroundColor, 2000);
+    addEventListeners();
 }
 
 function changeBackgroundColor() {
     document.body.style.background = 'lightblue';
+}
+
+function addEventListeners() {
+    const button = document.getElementById('cancel');
+    button.addEventListener('click', cancelBackgroundChange);
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeoutRef);
 }
